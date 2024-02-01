45ab251b7604677f72f8ec657e335909bc3c6648
klymovy4 <klymovy4roman@gmail.com>
2024-02-01 23:47:57 +0200

    first commit

---
+++ client/README.md
@@ -0,0 +1,17 @@
+# Getting Started with React Chat App
+
+
+## 1
+
+Download it
+
+### 2
+
+In `client` folder do `npm i` then  `npm start`
+
+### 3
+In `server` folder do `npm i` then  `npm start`
+
+### 4
+
+Open two browsers on `localhost:3000` and enjoy
---
+++ client/src/components/Messages/Messages.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Messages.css'
+import Message from "./Message/Message";
+
+const Messages = ({messages, name}) => (
+    <div className='messages'>
+      {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
+    </div>
+)
+export default Messages
